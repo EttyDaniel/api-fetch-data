165ca3f6d568a161617273d9f2f4bb799b5b0cbd
EttyDaniel <danieleti@gmail.com>
2022-03-29 00:02:59 -0400

    query string params extracted

--- index.js
+++ index.js
@@ -10,16 +10,35 @@ const port = process.env.PORT || 3000;
 app.use(bodyparser.urlencoded({extended: false }));
 app.use(bodyparser.json());
 
-app.get('/', (_,res) => {
-  res.send("My Server is running! ");
-});
+const posts = require("./Routes/posts");
+const params = {
+  tag: 'tech'
+};
 
-app.get('/api/posts', (_,res) => {
-  fetch('https://api.hatchways.io/assessment/blog/posts?tag=tech')
-  .then(response => response.json())
-  .then(data => res.send(data));
-});
+const url = new URL('https://api.hatchways.io/assessment/blog/posts');
+url.search = new URLSearchParams(params).toString();
 
+app.use('/',posts());
+// app.get('/', (_,res) => {
+//   res.send("My Server is running! ");
+// });
+
+// app.get('/api/posts', (req, res) => {
+//   fetch(url)
+//   .then(response => response.json())
+//   .then(data => res.send(data));
+// });
+
+app.get('/api/posts', (req, res) => {
+  console.log(req.url);
+  const actualUrl = 'localhost:3000' + req.url;
+  let url = new URL(actualUrl);
+  //var url = new URL('https://example.com?foo=1&bar=2');
+  const params = new URLSearchParams(url.search);
+  console.log(params);
+
+  res.send("print something")
+});
 
 app.listen(port,() => {
   console.log(`Listening on port: ${port}`);
